31cb31cce62aa4538508723bf4dc73de6a25b817
Asuni Tarmizi <155244811+sunio03@users.noreply.github.com>
2025-10-29 19:02:42 +0900

    Implement even_list functionImplement even_list function
    Implemented the even_list function to return even numbers.

--- main.py
+++ main.py
@@ -2,17 +2,7 @@ from typing import List
 
 # Skeleton code for even_list
 def even_list(int_list: List[int]) -> List[int]:
-    """
-    Determines if a number is even and returns a list of even numbers.
-
-    Args:
-        int_list: A list of integers.
-
-    Returns:
-        A list of even integers.
-    """
-    # TODO: Implement even_list
-    pass
+    return [num for num in int_list if num % 2 == 0]
 
 
 # Skeleton code for sum_of_squares_of_even
